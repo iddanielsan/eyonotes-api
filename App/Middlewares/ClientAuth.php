d4e0c91aa0dc4f9ea7637b1c519980b2fcfa74d7
iddaniellima <iddaniellima@outlook.com>
2019-07-10 23:07:07 -0300

    testes

--- App/Middlewares/ClientAuth.php
+++ App/Middlewares/ClientAuth.php
@@ -6,14 +6,11 @@ use App\Views\Status as Status;
 use Illuminate\Database\Capsule\Manager as DB;
 
 class ClientAuth{
-
 	public function init(){
-        if(!isset(app('request')->headers["eyoclientkey"])){
-    		status::render_error(401, "client", "client_key_not_received");
-    	} 
-
-    	else if(!DB::table("eyo_clients")->where('client_key', app('request')->headers["eyoclientkey"])->exists()){
-    		status::render_error(401, "client", "client_key_does_not_exist");
-    	}
+		if(!isset(app('request')->headers["eyoclientkey"])){
+			status::render_error(401, "client", "client_key_not_received");
+		} else if(!DB::table("eyo_clients")->where('client_key', app('request')->headers["eyoclientkey"])->exists()){
+			status::render_error(401, "client", "client_key_does_not_exist");
+		}
 	}
 }
