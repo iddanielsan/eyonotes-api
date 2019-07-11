7b8e40163a6444473bf7f7c58c6f16db56a6a92a
iddaniellima <iddaniellima@outlook.com>
2019-07-10 23:03:27 -0300

    Testes

--- App/Middlewares/ClientAuth.php
+++ App/Middlewares/ClientAuth.php
@@ -1,22 +1,19 @@
 <?php
 
 namespace App\Middlewares;
+
 use App\Views\Status as Status;
-use App\Classes\Security\Communication as Communication;
 use Illuminate\Database\Capsule\Manager as DB;
 
 class ClientAuth{
-    private $signature;
-    
-	public function init(){
-        $this->signature = Communication::decode(app('request')->headers['eyosignature']);
-        
-        if(!$this->signature){
-            Status::render_error(401, "client", "signature_unreadable");
-        } else if(!DB::table("eyo_clients")->where('client_key', json_decode($this->signature, true)['key'])->exists()){
-            status:render_error(401, "client", "invalid_signature");
-        }
-	}
-    
 
+	public function init(){
+        if(!isset(app('request')->headers["eyoclientkey"])){
+    		status::render_error(401, "client", "client_key_not_received");
+    	} 
+
+    	else if(!DB::table("eyo_clients")->where('client_key', app('request')->headers["eyoclientkey"])->exists()){
+    		status::render_error(401, "client", "client_key_does_not_exist");
+    	}
+	}
 }
