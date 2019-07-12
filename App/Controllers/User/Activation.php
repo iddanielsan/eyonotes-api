3a112d810f8d227526689358a9992f3a8f587d75
e4daniellima <e3daniellima@protonmail.com>
2019-07-12 22:36:57 +0000

    Correções de desempenho

--- App/Controllers/User/Activation.php
+++ App/Controllers/User/Activation.php
@@ -8,13 +8,15 @@ use App\Classes\Security\Communication as Communication;
 class Activation{
     private $req;
     private $uid;
+    private $code;
+    private $password;
     
     public function init(){
         if(!isset(app('request')->body['data'])){
 			Status::render_error(400, "client", "invalid_request");
-		} else{
-			$this->req = app('request')->body['data'];
 		}
+        
+        $this->req = app('request')->body['data'];
         
         $validate = new \Particle\Validator\Validator;
         $validate->required('code')->string();
@@ -23,12 +25,53 @@ class Activation{
         
         if(!$result->isValid()){
 			Status::render_error(400, "client", json_encode($result->getMessages()));
-		} else if($this->encryption_is_not_valid($this->req)){
+		} 
+        
+        if($this->encryption_is_not_valid($this->req)){
 			Status::render_error(400, "client", "invalid_encryption_method");
-		} else if(!$this->check_code()["status"]){
+		}
+        
+        $this->code = Communication::decode($this->req['code']);
+        $this->password = Communication::decode($this->req['user_password']);
+        
+        if(!$this->check_code()["status"]){
             Status::render_error(400, $this->check_code()["class"], $this->check_code()["message"]);
+        }
+    }
+    
+    
+    /*
+    * Checa se o código de ativação é valido.
+    */
+    private function check_code(){
+        
+        // Verifica se o código existe
+        if(!DB::table("eyo_activations_code")->where('code', $this->code)->exists()){
+            return array("message" => "code_does_not_exist", "class" => "user", "status" => false);
+            
+        // Verifica se o código já expirou.
+		} else if(time() > DB::table("eyo_activations_code")->where('code', $this->code)->value('duration')){
+            return array("message" => "time_has_expired", "class" => "user", "status" => false);
+            // Criar um novo?? (Futuro)
         } else{
-            echo "okay";
+            $this->uid = DB::table("eyo_activations_code")->where('code', $this->code)->value('uid');
+            
+            // Verifica se a conta pertecente ao código existe.
+            if(!DB::table("eyo_accounts")->where('id', $this->uid)->exists()){
+                return array("message" => "user_does_not_exist", "class" => "client", "status" => false);
+                
+            // Verifica se a conta já foi ativada
+            } else if(DB::table("eyo_accounts")->where('id', $this->uid)->value('account_status') !== "x1"){
+                return array("message" => "account_already_activated", "class" => "user", "status" => false);
+                
+            // Verifica a senha
+            } else if(!password_verify($this->password, DB::table("eyo_accounts")->where('id', $this->uid)->value('user_password'))){
+                return array("message" => "invalid_password", "class" => "user", "status" => false);
+                
+            // Tudo Ok
+            } else{
+                return array("status" => true);
+            }
         }
     }
     
@@ -41,47 +84,5 @@ class Activation{
 				return true;
 			}
 		}
-	}
-    
-    /*
-    * Checa se o código de ativação é valido.
-    */
-    private function check_code(){
-        
-        // Verifica se o código existe
-        if(!DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->exists()){
-            return array("message" => "code_does_not_exist", "class" => "user", "status" => false);
-            
-        // Verifica se o código já expirou.
-		} else if(time() > DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->value('duration')){
-            return array("message" => "time_has_expired", "class" => "user", "status" => false);
-            // Criar um novo?? (Futuro)
-        } else{
-            $this->uid = DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->value('uid');
-            
-            // Verifica se a conta pertecente ao código existe.
-            if(!DB::table("eyo_accounts")->where('id', $this->uid)->exists()){
-                return array("message" => "user_does_not_exist", "class" => "client", "status" => false);
-                
-            // Verifica se a conta já foi ativada
-            } else if(DB::table("eyo_accounts")->where('id', $this->uid)->value('account_status') !== "x1"){
-                return array("message" => "account_already_activated", "class" => "user", "status" => false);
-                
-            // Verifica a senha
-            } else if(!password_verify($this->dec($this->req['user_password']), DB::table("eyo_accounts")->where('id', $this->uid)->value('user_password'))){
-                return array("message" => "invalid_password", "class" => "user", "status" => false);
-                
-            // Tudo Ok
-            } else{
-                return array("status" => true);
-            }
-        }
-    }
-
-	/*
-	* Apenas para encurtar.
-	*/
-	private function dec($p){
-		return Communication::decode($p);
 	}
 }
