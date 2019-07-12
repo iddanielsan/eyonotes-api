c8d9b98ed52a28a3222286c025038d904be020ae
e4daniellima <e3daniellima@protonmail.com>
2019-07-12 21:08:43 +0000

    Finalizando

--- App/Controllers/User/Activation.php
+++ App/Controllers/User/Activation.php
@@ -7,6 +7,7 @@ use App\Classes\Security\Communication as Communication;
 
 class Activation{
     private $req;
+    private $uid;
     
     public function init(){
         if(!isset(app('request')->body['data'])){
@@ -18,12 +19,17 @@ class Activation{
         $validate = new \Particle\Validator\Validator;
         $validate->required('code')->string();
         $validate->required('user_password')->string();
+        $result = $validate->validate($this->req);
         
         if(!$result->isValid()){
 			Status::render_error(400, "client", json_encode($result->getMessages()));
 		} else if($this->encryption_is_not_valid($this->req)){
 			Status::render_error(400, "client", "invalid_encryption_method");
-		}
+		} else if(!$this->check_code()["status"]){
+            Status::render_error(400, $this->check_code()["class"], $this->check_code()["message"]);
+        } else{
+            echo "okay";
+        }
     }
     
 	/*
@@ -40,20 +46,34 @@ class Activation{
     /*
     * Checa se o código de ativação é valido.
     */
-    
     private function check_code(){
+        
+        // Verifica se o código existe
         if(!DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->exists()){
-            return array("message" => "code_does_not_exist", "status" => false);
-		} else if(DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->value('duration') > time()){
-            return array("message" => "time_has_expired", "status" => false);
-            // Criar um novo??
-        } else{
-            $uid = DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->value('uid');
+            return array("message" => "code_does_not_exist", "class" => "user", "status" => false);
             
-            if(!DB::table("eyo_accounts")->where('id', $uid)->exists()){
-                return array("message" => "user_does_not_exist", "status" => false);
-            } else if(DB::table("eyo_accounts")->where('id', $uid)->value('account_status') !== 1){
-                return array("message" => "account_already_activated", "status" => false);
+        // Verifica se o código já expirou.
+		} else if(time() > DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->value('duration')){
+            return array("message" => "time_has_expired", "class" => "user", "status" => false);
+            // Criar um novo?? (Futuro)
+        } else{
+            $this->uid = DB::table("eyo_activations_code")->where('code', $this->dec($this->req['code']))->value('uid');
+            
+            // Verifica se a conta pertecente ao código existe.
+            if(!DB::table("eyo_accounts")->where('id', $this->uid)->exists()){
+                return array("message" => "user_does_not_exist", "class" => "client", "status" => false);
+                
+            // Verifica se a conta já foi ativada
+            } else if(DB::table("eyo_accounts")->where('id', $this->uid)->value('account_status') !== "x1"){
+                return array("message" => "account_already_activated", "class" => "user", "status" => false);
+                
+            // Verifica a senha
+            } else if(!password_verify($this->dec($this->req['user_password']), DB::table("eyo_accounts")->where('id', $this->uid)->value('user_password'))){
+                return array("message" => "invalid_password", "class" => "user", "status" => false);
+                
+            // Tudo Ok
+            } else{
+                return array("status" => true);
             }
         }
     }
--- App/init.php
+++ App/init.php
@@ -1,9 +1,10 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header('Content-Type: application/json');
 
-define('DS', DIRECTORY_SEPARATOR, true);
-define('BASE_PATH', dirname(dirname(__FILE__)).DS, TRUE);
+define('DS', DIRECTORY_SEPARATOR);
+define('BASE_PATH', dirname(dirname(__FILE__)).DS);
 
 require('config.php');
 require BASE_PATH.'vendor/autoload.php';
